265d6c2f04a9f8f7cecdaf1c46e3cb46821808cc
Thiti Jantamongkollert <gera.tht@gmail.com>
2017-11-27 14:25:50 +0700

    27112017
    Push button to Show data
    //Auto show data

--- app/src/main/java/com/example/gear/asrm/MainActivity.java
+++ app/src/main/java/com/example/gear/asrm/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.gear.asrm;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +18,9 @@ import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
+import static android.bluetooth.le.ScanSettings.SCAN_MODE_LOW_LATENCY;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -45,11 +50,16 @@ public class MainActivity extends AppCompatActivity{
         Log.e(TAG, "APP STARTED UP NOW!!!!!!!!!!!!!!!!!!!!!!!!!");
 
         final TextView op_find = (TextView) findViewById(R.id.output_found);
+        final TextView op_kneeL = (TextView) findViewById(R.id.output_kneeL);
+        final TextView op_ankleL = (TextView) findViewById(R.id.output_ankleL);
 
 /* UI Binding */
         //Define Button
         Button button_showData = (Button) findViewById(R.id.button_getData);
         Button button_findAngel = (Button) findViewById(R.id.button_findAngle);
+
+
+
 
 
         /* DeviceList Structure
@@ -82,19 +92,14 @@ public class MainActivity extends AppCompatActivity{
                 */
 
         /* Get Body */
-                double arm = core.calculateDistance(deviceList.get("50:8C:B1:75:16:D2")[0], deviceList.get("50:8C:B1:75:16:D2")[1]);
-                Log.e(TAG, "///////////////////////Arm :" + arm);
-                double knee = core.calculateDistance(deviceList.get("D4:36:39:DE:56:C6")[0], deviceList.get("D4:36:39:DE:56:C6")[1]);
+                //double arm = core.calculateDistance(deviceList.get("50:8C:B1:75:16:D2")[0], deviceList.get("50:8C:B1:75:16:D2")[1]);
+                //Log.e(TAG, "///////////////////////Arm :" + arm);
+                double knee = core.calculateDistance(deviceList.get("50:8C:B1:75:1C:3C")[0], deviceList.get("50:8C:B1:75:1C:3C")[1]);
                 Log.e(TAG, "///////////////////////Knee :" + knee);
-                double ankle = core.calculateDistance(deviceList.get("D4:36:39:DE:57:D0")[0], deviceList.get("D4:36:39:DE:57:D0")[1]);
+                double ankle = core.calculateDistance(deviceList.get("D4:36:39:DE:57:D0")[0], (deviceList.get("D4:36:39:DE:57:D0")[1]) - 20);
                 Log.e(TAG, "///////////////////////Ankle :" + ankle);
                 double shin = 40.0;
-                setTextTextViewLeft(core.getBodyHalf(
-                        core.calculateDistance(deviceList.get("50:8C:B1:75:16:D2")[0], deviceList.get("50:8C:B1:75:16:D2")[1])
-                        , core.calculateDistance(deviceList.get("D4:36:39:DE:56:C6")[0], deviceList.get("D4:36:39:DE:56:C6")[1])
-                        , core.calculateDistance(deviceList.get("D4:36:39:DE:57:D0")[0], deviceList.get("D4:36:39:DE:57:D0")[1])
-                        , shin
-                ));
+                setTextTextViewLeft(core.getBodyHalf(999,knee, ankle, shin));
 /*
                 if (count == 0) {
                     body = core.getBody1();
@@ -122,28 +127,28 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
 
-                op_find.setText("");
+
             }
         });
 
 
 /* Bluetooth Scan to Find Beacon */
+        ScanSettings.Builder scanSettingsBuilder = new ScanSettings.Builder();
+        scanSettingsBuilder.setReportDelay(0);
+
         bluetoothLeScanner.startScan(new ScanCallback() {
             @Override
             public void onScanResult(int callbackType, ScanResult result) {
                 super.onScanResult(callbackType, result);
+
+                op_find.setText("" + deviceList.size());
                 scanResult = result;
 
                 if (scanResult.getDevice().getName() != null) {
-                    int[] capsuleRssiTxPower = {scanResult.getRssi(), scanRecord.getTxPowerLevel()};
+                    int[] capsuleRssiTxPower = {scanResult.getRssi(), scanResult.getScanRecord().getTxPowerLevel()};
 
-/*                  Log.e(TAG, "******************* Device Key : " +
+                    Log.e(TAG, "******************* Device Key : " +
                             deviceList.keySet());
-                    Log.e(TAG, "******************* Device : " +
-                            scanResult.getDevice().getAddress());
-                    Log.e(TAG, "************** Distance :" +
-                            core.calculateDistance(scanResult.getRssi(), scanRecord.getTxPowerLevel())); */
-
                     if (deviceList.containsKey(scanResult.getDevice().getAddress())) {
                         /* Update Device Rssi & TxPowerLevel */
                         deviceList.put(scanResult.getDevice().getAddress(), capsuleRssiTxPower);
@@ -152,9 +157,31 @@ public class MainActivity extends AppCompatActivity{
                         /* Add Device to DeviceList */
                         deviceList.put(scanResult.getDevice().getAddress(), capsuleRssiTxPower);
                     }
+/*
+                    if (deviceList.size() >= 3) {
+                        long start = System.currentTimeMillis();
+                        long elapsedTimeMillis = System.currentTimeMillis()-start;
+                        while(elapsedTimeMillis < 2){
+                            //op_kneeL.setText("" + core.calculateDistance(deviceList.get("D4:36:39:DE:56:C6")[0], deviceList.get("D4:36:39:DE:56:C6")[1]));
+                            op_kneeL.setText("" + core.calculateDistance(deviceList.get("50:8C:B1:75:1C:3C")[0], deviceList.get("50:8C:B1:75:1C:3C")[1]));
+                            op_ankleL.setText("" + core.calculateDistance(deviceList.get("D4:36:39:DE:57:D0")[0], deviceList.get("D4:36:39:DE:57:D0")[1]-20));
+                            start = System.currentTimeMillis();
+                        }
+
+                    }
+*/
                 }
             }
         });
+
+        /*
+        * Intent intent = new Intent(this, MainActivity);
+        * // IMPORTANT: in the AndroidManifest.xml definition of this activity, you must set android:launchMode="singleInstance" or you will get two instances
+        * // created when a user launches the activity manually and it gets launched from here.
+        * intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        * this.startActivity(intent);
+        */
+
     }
 
 
@@ -165,6 +192,8 @@ public class MainActivity extends AppCompatActivity{
         final TextView op_ankleL = (TextView) findViewById(R.id.output_ankleL);
         final TextView op_shinL = (TextView) findViewById(R.id.output_shinL);
         final TextView op_angleL = (TextView) findViewById(R.id.output_angleL);
+
+        //final  TextView op_found =
 
         double angleL = core.findAngle(body[1], body[2], body[3]);
 
@@ -196,6 +225,7 @@ public class MainActivity extends AppCompatActivity{
         getMenuInflater().inflate(R.menu.menu_beacon, menu);
         return true;
     }
+
     /* Select MENU ITEM */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
